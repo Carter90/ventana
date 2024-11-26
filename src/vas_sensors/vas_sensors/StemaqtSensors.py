b370b6da275cc088e35454e16fc7ef8ca6d35d92
Carter Frost <git@carterfrost.com>
2024-11-25 22:25:32 -0800

    src/vas_control|sensors|user_interface: packages creation

---
+++ src/vas_sensors/vas_sensors/StemaqtSensors.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from vas_sensors.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/vas_user_interfaces/vas_user_interfaces/paperDisplay.py
@@ -0,0 +1,3 @@
+# paired with an encoder and its buttons
+if __name__ == '__main__':
+    pass
